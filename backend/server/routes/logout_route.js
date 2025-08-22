3d178c5a614a899990ca6cb8c318d75a11aaafcf
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-08-22 17:00:53 +0530

    fix for deployment -14 added secure and same site in logout route in clearCookie

--- backend/server/routes/logout_route.js
+++ backend/server/routes/logout_route.js
@@ -6,7 +6,7 @@ const logout = express();
 
 logout.post("/", (req, res) => {
 
-    res.clearCookie('token', { httpOnly: true });
+    res.clearCookie('token', { httpOnly: true, secure: true, sameSite: "none" });
     res.json({ message: "Logged out successfully" });
 
 });
