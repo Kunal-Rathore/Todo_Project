fed134fb23c86a6d5ffcf8cfab8d76f08191d23c
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-08-22 15:03:24 +0530

    fix for deployment -7 add the origin url in cors

--- backend/server/index.js
+++ backend/server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "*",
+    origin: "https://todo-project-kohl.vercel.app/",
     credentials: true,
 }));
 
