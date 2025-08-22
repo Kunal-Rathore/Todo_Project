0a5e21bdd27ffc124adf65fdca211405f75487d6
Kunal-Rathore <kunalworkspace111@gmail.com>
2025-08-22 14:10:49 +0530

    fixed the vercel.json

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,11 +1,44 @@
+require("dotenv").config();
+
+const rateLimit = require("express-rate-limit");
 const express = require("express");
+const cors = require("cors");
+const signup_in = require("./routes/sign_routes");
+const todos = require("./routes/todos_routes");
+const logout = require("./routes/logout_route");
+const checkToken = require("./routes/checkToken_route");
+const cookieParser = require("cookie-parser");
+
 const serverless = require("serverless-http");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server activated ✅");
-});
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
+
+app.use(rateLimit({
+    windowMs: 1000 * 60 * 15, max: 400,
+    message: {
+        message: "To many requests from the same Ip try again after 15 mints"
+    },
+    skip: (req) => req.method === "OPTIONS" // skip the rateLimit for OPTIONS method
+})); // limit only 25 request per ip, in 15mints
+
+
+app.use('/issignedin', checkToken);   // to check does token exists
+app.use('/sign', signup_in);         // route which handels signup and signin 
+app.use('/todos', todos);           // route which handels todos operations 
+app.use('/logout', logout);
+
+app.get('/', (req, res) => {
+    res.send("Server activated");
+})
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
